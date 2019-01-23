323389ef4f3276fddafc633b0a59e2cd482d6963
Rodrik Klesse <rodrik_hsk@hotmail.com>
2019-01-22 23:21:33 -0200

    Inserindo usuarios com POST

--- src/main/java/com/rodrikklesse/springbootmongodb/services/UserService.java
+++ src/main/java/com/rodrikklesse/springbootmongodb/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.rodrikklesse.springbootmongodb.domain.User;
+import com.rodrikklesse.springbootmongodb.dto.UserDTO;
 import com.rodrikklesse.springbootmongodb.repository.UserRepository;
 import com.rodrikklesse.springbootmongodb.services.exception.ObjectNotFoundException;
 
@@ -22,7 +23,14 @@ public class UserService {
 	
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
-		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
-		
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));		
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
 }
